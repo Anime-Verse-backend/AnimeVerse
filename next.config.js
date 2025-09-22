923fd9fa8e1d4821ee282bd373b139a1f5362519
MIGUEL6-BNX <luismiguel10608@gmail.com>
2025-09-22 16:16:55 -0500

    SOLUCION 441

--- next.config.js
+++ next.config.js
@@ -38,6 +38,10 @@ const nextConfig = {
         hostname: '*.onrender.com',
         port: '',
         pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
       }
     ],
   },
